bfd21ef738544c7f597b8fd1b6bfdd62a817818b
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2024-10-16 12:19:48 +0300

    mvc

---
+++ Form Page/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Form_Page.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
