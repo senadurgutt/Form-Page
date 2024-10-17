a7525b40228d36b34104394b3a09fa210e26a70a
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2024-10-17 17:49:07 +0300

    category filtrelemesi de yapıldı

--- Form Page/Controllers/HomeController.cs
+++ Form Page/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Form_Page.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace Form_Page.Controllers
@@ -13,18 +14,34 @@ namespace Form_Page.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(string searchString) //searchString  ürünler içinde arama yapmak için kullanılır
+        public IActionResult Index(string searchString, string category) //searchString  ürünler içinde arama yapmak için kullanılır
         {
             var products = Repository.Products;  //products, ürünleri saklayan bir değişkendir ve Repository.Products üzerinden alınır.
+            
             if (!String.IsNullOrEmpty(searchString))
             {
+                ViewBag.SearchString = searchString; //arama yapılan kelimenin arama butonunda kalması için kullanılıyor
                 products = products.Where(p=> p.Name.Contains(searchString)).ToList();
 
 //                ürün listesini filtreler.products içindeki her bir ürünün Name(isim) özelliği, kullanıcının girdiği searchString ifadesini içeriyorsa, o ürün listede kalır.
 //                Contains(searchString): Arama terimi, ürün isminin içinde geçiyor mu diye kontrol eder.
 //                ToList(): Sonucu bir listeye dönüştürür.
+            }
+
+            if (!String.IsNullOrEmpty(category) && category != "0") 
+            {
+                products = products.Where(p=>p.CategoryId ==int.Parse(category)).ToList();
+
+//                String.IsNullOrEmpty(category): Bu ifade, category değişkeninin boş(empty) veya null olup olmadığını kontrol eder. Eğer category boş değilse, kod çalışmaya devam eder.
+//                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();:
+
+//                products: Ürünlerin yer aldığı bir koleksiyondur.Bu ürünler muhtemelen bir veritabanından alınmıştır ve CategoryId alanına sahiptir(her ürünün bir kategori kimliği var).
+//                products.Where(...): LINQ sorgusu kullanılarak ürünler filtrelenir.Sorgu, CategoryId'si, kullanıcının seçtiği category değerine eşit olan ürünleri bulur.
+//                p.CategoryId == int.Parse(category): Bu ifade, her bir ürünün(p) CategoryId özelliği ile seçilen kategori değerini karşılaştırır.Kategori değeri bir string olarak geldiğinden, int.Parse(category) ifadesiyle sayıya dönüştürülür ve CategoryId ile kıyaslanır.
 
             }
+
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
             return View(products);
         }
 
