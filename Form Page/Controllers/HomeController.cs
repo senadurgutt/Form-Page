3f8d6ca40c554bcdaa5e3465462e11aff517fe58
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2024-10-22 14:57:23 +0300

    image file upload

--- Form Page/Controllers/HomeController.cs
+++ Form Page/Controllers/HomeController.cs
@@ -18,26 +18,26 @@ namespace Form_Page.Controllers
         public IActionResult Index(string searchString, string category) //searchString  ürünler içinde arama yapmak için kullanılır
         {
             var products = Repository.Products;  //products, ürünleri saklayan bir değişkendir ve Repository.Products üzerinden alınır.
-            
+
             if (!String.IsNullOrEmpty(searchString))
             {
                 ViewBag.SearchString = searchString; //arama yapılan kelimenin arama butonunda kalması için kullanılıyor
-                products = products.Where(p=> p.Name!.Contains(searchString)).ToList();
+                products = products.Where(p => p.Name!.Contains(searchString)).ToList();
 
-//                ürün listesini filtreler.products içindeki her bir ürünün Name(isim) özelliği, kullanıcının girdiği searchString ifadesini içeriyorsa, o ürün listede kalır.
-//                Contains(searchString): Arama terimi, ürün isminin içinde geçiyor mu diye kontrol eder.
-//                ToList(): Sonucu bir listeye dönüştürür.
+                //                ürün listesini filtreler.products içindeki her bir ürünün Name(isim) özelliği, kullanıcının girdiği searchString ifadesini içeriyorsa, o ürün listede kalır.
+                //                Contains(searchString): Arama terimi, ürün isminin içinde geçiyor mu diye kontrol eder.
+                //                ToList(): Sonucu bir listeye dönüştürür.
             }
 
-            if (!String.IsNullOrEmpty(category) && category != "0") 
+            if (!String.IsNullOrEmpty(category) && category != "0")
             {
-                products = products.Where(p=>p.CategoryId == int.Parse(category)).ToList();
+                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
 
-//                String.IsNullOrEmpty(category): Bu ifade, category değişkeninin boş(empty) veya null olup olmadığını kontrol eder. Eğer category boş değilse, kod çalışmaya devam eder.
-//                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();:
-//                products: Ürünlerin yer aldığı bir koleksiyondur.Bu ürünler muhtemelen bir veritabanından alınmıştır ve CategoryId alanına sahiptir(her ürünün bir kategori kimliği var).
-//                products.Where(...): LINQ sorgusu kullanılarak ürünler filtrelenir.Sorgu, CategoryId'si, kullanıcının seçtiği category değerine eşit olan ürünleri bulur.
-//                p.CategoryId == int.Parse(category): Bu ifade, her bir ürünün(p) CategoryId özelliği ile seçilen kategori değerini karşılaştırır.Kategori değeri bir string olarak geldiğinden, int.Parse(category) ifadesiyle sayıya dönüştürülür ve CategoryId ile kıyaslanır.
+                //                String.IsNullOrEmpty(category): Bu ifade, category değişkeninin boş(empty) veya null olup olmadığını kontrol eder. Eğer category boş değilse, kod çalışmaya devam eder.
+                //                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();:
+                //                products: Ürünlerin yer aldığı bir koleksiyondur.Bu ürünler muhtemelen bir veritabanından alınmıştır ve CategoryId alanına sahiptir(her ürünün bir kategori kimliği var).
+                //                products.Where(...): LINQ sorgusu kullanılarak ürünler filtrelenir.Sorgu, CategoryId'si, kullanıcının seçtiği category değerine eşit olan ürünleri bulur.
+                //                p.CategoryId == int.Parse(category): Bu ifade, her bir ürünün(p) CategoryId özelliği ile seçilen kategori değerini karşılaştırır.Kategori değeri bir string olarak geldiğinden, int.Parse(category) ifadesiyle sayıya dönüştürülür ve CategoryId ile kıyaslanır.
 
             }
 
@@ -60,16 +60,35 @@ namespace Form_Page.Controllers
 
 
         [HttpPost]
-        public IActionResult Create(Product model, IFormFile imageFile)
+        public async Task<IActionResult> Create(Product model, IFormFile imageFile) //productlar model içerisine kayıt edildiği için form file için başka bir şey açman gerekiyordu onu da böyle kaydetmek için bu şekilde yazdık
         {
-            if (ModelState.IsValid)  /*isvalid özelliği product modeldeki her şeyin kuralına uygun gelip gelmediğini kontrol eder*/
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png"};
+            var extension = Path.GetExtension(imageFile.FileName);
+            var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");  // extension yerine .jpg de yazabilirdin 
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName); //Directory.GetCurrentDirectory() kalıp olarak bulunduğum dizine gelir root olarak da görseller wwwroot/img altında olduğu için arkasından da kaydedilmesi için dosya ismini alıyoruz
+
+            if (imageFile != null)
             {
-                model.ProductId = Repository.Products.Count + 1;
-                Repository.CreateProduct(model);
-                return RedirectToAction("Index"); // sayfanın view i çalışmıyor da index sayfasını döndürüyor.
+                if (!allowedExtensions.Contains(extension))
+                {
+                    ModelState.AddModelError("", "Geçerli resim seçiniz");
+                }
             }
-            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
-            return View(model); //eğer her şey yolunda değilse aynı sayfa yazdıklarıyla birlikte tekrar ona dönecek
+            if (ModelState.IsValid)  /*isvalid özelliği product modeldeki her şeyin kuralına uygun gelip gelmediğini kontrol eder*/
+                {
+                    using (var stream = new FileStream(path, FileMode.Create)) //using ifadesi doşya akışı açılır ve kullanıldıktan sonra kapanır
+                    { //filestream dosyaya veri yazmak ve okumak için kullanılr
+                        await imageFile.CopyToAsync(stream);
+                    }
+
+                    model.Image = randomFileName;
+                    model.ProductId = Repository.Products.Count + 1;
+                    Repository.CreateProduct(model);
+                    return RedirectToAction("Index"); // sayfanın view i çalışmıyor da index sayfasını döndürüyor.
+                }
+                ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+                return View(model); //eğer her şey yolunda değilse aynı sayfa yazdıklarıyla birlikte tekrar ona dönecek
+            }
         }
-    }
+    
 }
